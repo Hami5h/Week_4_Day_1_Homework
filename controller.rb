529467080541032b6db73958447f61138ae73064
Hamish <hamishfpso@gmail.com>
2017-10-16 16:39:00 +0100

    Part_1 fixed for draw scenario

--- controller.rb
+++ controller.rb
@@ -25,3 +25,15 @@ end
 get '/scissors/paper' do
   return "scissors wins"
 end
+
+get '/scissors/scissors' do
+  return "It's a draw"
+end
+
+get '/rock/rock' do
+  return "It's a draw"
+end
+
+get '/paper/paper' do
+  return "it's a draw"
+end
